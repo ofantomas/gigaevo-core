593df651bb9fcf73d14fb557b9a0c7ce4c4fa110
danilkafish <fisher.de19@physics.msu.ru>
2025-10-22 08:45:44 +0300

    greedy+opt+tohpe GF2 64 v1.0

--- problems/quantum_circuits/context.py
+++ problems/quantum_circuits/context.py
@@ -16,16 +16,16 @@ def build_context() -> Data:
             shape = f[file].shape
             # if rank * n < 1000:
             print(file)
-            name = "gf_2pow6_mult"
-            if file == name + "_comp1":
+            name = "gf_2pow6_mult_comp1"
+            if file == name:
                 print(f[file])
                 l.append(Data(file, 
                               ParityMatrix(f[file][0,:,:].reshape(rank, n).T), 
                               ParityMatrix(np.load(f"data/{name}.matrix.npy")),
                             rank))
-    # print(np.argwhere(l[0].sota_decomposition.T != l[0].early_decomposition.T).shape[0])
+    print(np.argwhere(l[0].sota_decomposition.T != l[0].early_decomposition.T).shape)
     # print(l[0].early_decomposition.P.shape)
     # print(l[0].sota_decomposition.P.shape)
     return l[0]
 
-# build_context()
+build_context()
